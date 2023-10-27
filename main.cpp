2c153b46c5994221fcfa34dd392b994f73c3e7e6
rrivas <rrivas@utec.edu.pe>
2023-10-27 08:52:16 -0500

    Uso de future y async (Ejercicio 3)

--- main.cpp
+++ main.cpp
@@ -4,6 +4,7 @@
 #include <numeric>
 #include <chrono>
 #include <thread>
+#include <future>
 using namespace std;
 
 void ejercicio_1() {
@@ -61,6 +62,46 @@ void ejercicio_2() {
 
     // Calcular el total de los resultados
     auto total = accumulate(begin(vr), end(vr), static_cast<long long int>(0));
+    cout << chrono::duration_cast<chrono::microseconds>(finish - start).count() << endl;
+    cout << total << endl;
+
+}
+
+void ejercicio_3() {
+    int n = 10000;
+    vector<int> vd(n);
+    // llenar el vector con valores desde 1 hasta n
+    iota(begin(vd), end(vd), 1);
+
+    auto start = chrono::high_resolution_clock::now();
+
+    // Cantidad de hilos
+    int nf = thread::hardware_concurrency();
+    if (n < 10000000) {
+        nf = 1;
+    }
+    cout << "Hilos de nucleo: " << nf << endl;
+
+    // Crear hilos
+    vector<future<int>> vf(nf);
+
+    // Distribuyendo las tareas
+    auto range = n / nf;
+
+    auto first = begin(vd);
+    for (int i = 0; i < nt - 1; ++i) {
+        auto last = next(first, range);
+        vf[i] = async(accumulate<decltype(first), int>, first, last, 0ll);
+        first = last;
+    }
+    vt.back() = async(accumulate<decltype(first), int>, first, end(vd), 0ll);
+    auto finish = chrono::high_resolution_clock::now();
+
+    // Calcular el total de los resultados usando futures
+    auto total = accumulate(begin(vf), end(vf), Oll, [](auto sum, auto& item){
+        sum += item.get();
+        return sum;
+    });
     cout << chrono::duration_cast<chrono::microseconds>(finish - start).count() << endl;
     cout << total << endl;
 
